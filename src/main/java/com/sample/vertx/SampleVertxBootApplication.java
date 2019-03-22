982a4210a7b56ed27adceb5e677d472b05d03e7d
smluv82 <smluv82@naver.com>
2019-03-22 14:04:02 +0900

    first commit

---
+++ src/main/java/com/sample/vertx/SampleVertxBootApplication.java
@@ -0,0 +1,13 @@
+package com.sample.vertx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleVertxBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleVertxBootApplication.class, args);
+	}
+
+}
